f84be86ace06fad38d28e5d17b3848be30a3aef8
Tanmay <tanmay123@gmail.com>
2025-11-26 11:56:47 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="min-h-screen bg-[#0F172A] text-white p-6">
       <h1 className="text-3xl font-bold text-white text-center mb-6">
-        Task Manager App
+        Task Manager
       </h1>
 
       <AddTaskForm />
